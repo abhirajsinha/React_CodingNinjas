e0b08e5627365a2d2676accef2fc5b8f7b95ebf4
Abhiraj Sinha <abhirajsinha25@gmail.com>
2021-06-10 12:43:32 +0530

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Cart from './Cart';
 import CartItem from './CartItem';
-
+import Navbar from './Navbar';
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Cart/>
-    </div>
+    </div> 
   );
 }
 
